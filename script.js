4a4ec634cb97e95901434c76bc8a15cfd5e17155
Santino Sottile <santinosottile80@gmail.com>
2023-10-20 18:51:23 +0200

    static model added

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)'); // costitu
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie'); // elemento html
+// prevedere la possibilità di scelta film vietati ai minori di 18 anni
+const model = [
+	{seats: [1, 2, 3, 4], id: 455784, title: "Film bellissimo", price: 0 }, // proprieta' oggetti
+	{seats: [5, 6, 7, 8], id: 455785, title: "Film per maniaci", price: 50  },
+	{seats: [9, 10, 11, 12], id: 455786, title: "Film noioso", price: 1  },
+	{seats: [13, 14, 15, 16], id: 455787, title: "Film orrido", price: 1089  },
+  {seats: [17], id: 455788, title: "Film per adulti maniaci", price: 187  }
+]
 
+// creare funzione che salva nel local storage saveModelToLocalStorage
+// creare funzione che riempie il modello presi dal local storage populateModelFromLocalStorage (se vuoto modello vuoto)
+// riempire il selector dei film (mettere id come valore devono essere oggetti option) e come testo nome e prezzo del //film
+// creare funzione populateSeats deve prendere i dati che corrispondono al film selezionato (proprieta' seats del modello) e marcare a videoi posti come selezionati
 populateUI();
 
 let ticketPrice = +movieSelect.value; // significato del +? movieSelect.value è una stringa
